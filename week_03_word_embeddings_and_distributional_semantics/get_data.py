023c3f81cda6c5a7aaab8ac93674dbacafd31546
Ryan McNamara <rjm2232@columbia.edu>
2025-05-19 21:14:29 -0700

    Create runner file get_data.py for collecting pubmed abstracts for skip-gram model

---
+++ week_03_word_embeddings_and_distributional_semantics/get_data.py
@@ -0,0 +1,26 @@
+# import libraries
+from Bio import Entrez, Medline
+import pandas as pd
+import numpy as np
+import time
+
+from src.data_utils import * 
+
+import os
+
+def download_abstracts(email): 
+    Entrez.email = email 
+    search_query = 'medicine[MeSH Terms]'
+
+    ids = search_abstracts(search_query, max_results = 10_000)
+    print(f'Found {len(ids)} articles matching the query.')
+    print('Fetching abstracts...')
+
+    abstracts = fetch_abstracts(ids, batch_size = 500)
+
+    abstracts_df = pd.DataFrame(abstracts)
+    abstracts_df = abstracts_df[abstracts_df['Abstract'] != 'No Abstract']
+
+    print('There are {} abstracts with text.'.format(len(abstracts_df)))
+
+    abstracts_df.to_csv('./data/pubmed_abstracts.csv', index = False, header = True)
