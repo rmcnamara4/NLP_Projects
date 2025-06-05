49b45235ee2e63b82aa63efbee0daea562f89ddd
Ryan McNamara <rjm2232@columbia.edu>
2025-06-04 18:28:04 -0700

    Initial imports and define main() function

--- week_05_transformer_architecture/main.py
+++ week_05_transformer_architecture/main.py
@@ -3,5 +3,19 @@ from src.data.preprocessing import numericalize, tokenize, build_vocab
 
 from src.model.transformer import TransformerModel 
 
-if __name__ == '__main__':
+from src.utils.config import load_config
+from src.utils.logging import setup_logging
+
+import os
+
+def main(): 
+    config = load_config('./src/config.yaml')
+    setup_logging(log_file = config['paths']['train_log'])
+
+    src_lang = config['dataset']['src_lang']
+    tgt_lang = config['dataset']['tgt_lang']
     
+    
+
+if __name__ == '__main__':
+    main()
