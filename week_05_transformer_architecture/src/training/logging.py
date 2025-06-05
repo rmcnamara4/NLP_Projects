cc436e72a15f712f9ccbec46d57bf34f976837b4
Ryan McNamara <rjm2232@columbia.edu>
2025-06-04 17:07:41 -0700

    Create logging.py to hold function for setting up logging

---
+++ week_05_transformer_architecture/src/training/logging.py
@@ -0,0 +1,15 @@
+import logging 
+import os
+
+def setup_logging(log_dir = './logs', log_file = 'train.log'): 
+    os.makedirs(log_dir, exist_ok = True) 
+    log_path = os.path.join(log_dir, log_file) 
+
+    logging.basicConfig(
+        level = logging.INFO, 
+        format = '%(asctime)s - [%(levelname)s] %(message)s', 
+        handlers = [
+            logging.FileHandler(log_path), 
+            logging.StreamHandler()
+        ]
+    )
