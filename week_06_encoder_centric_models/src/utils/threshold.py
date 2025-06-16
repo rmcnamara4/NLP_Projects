4ae5ca54dc6695f1fea2315d63c2d3a3e8c521de
Ryan McNamara <rjm2232@columbia.edu>
2025-06-15 17:36:52 -0700

    Add function to calculate threshold

---
+++ week_06_encoder_centric_models/src/utils/threshold.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import fbeta_score
+import json
+
+def find_best_threshold(probs, labels, metric = 'f1', step = 0.01, beta = 1): 
+    best_threshold = 0.5
+    best_score = -1
+
+    for thresh in np.arange(0, 1 + step, step): 
+        preds = (probs >= thresh).astype(int) 
+        score = fbeta_score(labels, preds, beta = beta) 
+        if score > best_score: 
+            best_score = score
+            best_threshold = thresh
+    return best_threshold, best_score 
+
+def save_threshold(threshold, path): 
+    with open(path, 'w') as f: 
+        json.dump({'threshold': threshold}, f) 
+
+def load_threshold(path):
+    with open(path, 'r') as f: 
+        data = json.load(f)
+    return data['threshold']
