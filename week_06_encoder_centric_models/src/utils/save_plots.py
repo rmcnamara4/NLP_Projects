2b915dc42ebb2ddfa19fa3562cdb40565f97b5c5
Ryan McNamara <rjm2232@columbia.edu>
2025-06-15 18:25:11 -0700

    Update

--- week_06_encoder_centric_models/src/utils/save_plots.py
+++ week_06_encoder_centric_models/src/utils/save_plots.py
@@ -7,7 +7,7 @@ import json
 
 def save_plots(y_true, y_pred_proba, y_pred, path = None, set = 'Train'): 
     metrics = calculate_metrics(y_true, y_pred_proba, y_pred, set = set) 
-    with open(os.path.join(path, f'/{set.lower()}_metrics.json'), 'w') as f: 
+    with open(os.path.join(path, f'{set.lower()}_metrics.json'), 'w') as f: 
         json.dump(metrics, f, indent = 4)
     plot_roc_curve(y_true, y_pred_proba, path = os.path.join(path, f'{set.lower()}_roc_curve.png'), set = set) 
     plot_pr_curve(y_true, y_pred_proba, path = os.path.join(path, f'{set.lower()}_auprc_curve.png'), set = set)
