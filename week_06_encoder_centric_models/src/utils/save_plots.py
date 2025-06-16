34dfb2347ecd5b5d855d6c82ca5c2e2a7aae5a8f
Ryan McNamara <rjm2232@columbia.edu>
2025-06-15 17:54:01 -0700

    Update save plot paths

--- week_06_encoder_centric_models/src/utils/save_plots.py
+++ week_06_encoder_centric_models/src/utils/save_plots.py
@@ -3,9 +3,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
 
 from metrics import * 
+import json
 
 def save_plots(y_true, y_pred_proba, y_pred, path = None, set = 'Train'): 
     metrics = calculate_metrics(y_true, y_pred_proba, y_pred, set = set) 
-    plot_roc_curve(y_true, y_pred_proba, path = path, set = set) 
-    plot_pr_curve(y_true, y_pred_proba, path = path, set = set)
-    create_confusion_matrix(y_true, y_pred, path = path, set = set)
+    with open(os.path.join(path, f'/{set.lower()}_metrics.json'), 'w') as f: 
+        json.dump(metrics, f, indent = 4)
+    plot_roc_curve(y_true, y_pred_proba, path = os.path.join(path, f'{set.lower()}_roc_curve.png'), set = set) 
+    plot_pr_curve(y_true, y_pred_proba, path = os.path.join(path, f'{set.lower()}_auprc_curve.png'), set = set)
+    create_confusion_matrix(y_true, y_pred, path = os.path.join(path, f'{set.lower()}_confusion_matrix.png'), set = set)
