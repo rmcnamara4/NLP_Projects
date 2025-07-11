4d3d23b3ad96bc28ad0dc17fee02058482a0e5ba
Ryan <rjm2232@columbia.edu>
2025-07-11 01:46:53 +0000

    Update

--- week_07_decoder_centric_models/scripts/train.py
+++ week_07_decoder_centric_models/scripts/train.py
@@ -1,21 +1,23 @@
 import hydra 
-from omegaconf import DictConfig, OmegaConf
-from hydra.utils import instantiate, call 
+from omegaconf import DictConfig
+from hydra.utils import instantiate
 
 from pytorch_lightning import Trainer 
 
 import warnings
 warnings.filterwarnings('ignore', message = 'pkg_resources is deprecated')
 
+from src.data.dataset import SummarizationDataModule
+from src.models.summarizer import SummarizationModule
 from transformers import AutoTokenizer
 
 ## DON'T FORGET TO SET SEED
-@hydra.main(config_path = 'configs', config_name = 'config', version_base = '1.3')
+@hydra.main(config_path = '../configs', config_name = 'config', version_base = '1.3')
 def main(cfg: DictConfig): 
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name)
     tokenizer.pad_token = tokenizer.eos_token 
 
-    data_module = instantiate(cfg.datamodule) 
+    data_module = SummarizationDataModule(cfg.datamodule, tokenizer = tokenizer)
 
     data_module.prepare_data()
     data_module.setup(stage = 'fit')
@@ -23,7 +25,7 @@ def main(cfg: DictConfig):
     train_loader = data_module.train_dataloader()
     val_loader = data_module.val_dataloader()
 
-    model = instantiate(cfg.model, tokenizer = tokenizer) 
+    model = SummarizationModule(cfg.model, tokenizer = tokenizer)
     callbacks = [instantiate(cb) for cb in cfg.callbacks]
 
     trainer = Trainer(
