c034aa460a2ff12198d40b84c2e3ad28e31a4fb1
Ryan McNamara <rjm2232@columbia.edu>
2025-07-12 12:21:34 -0700

    Update train.py to save final model and set seed at the top

--- week_07_decoder_centric_models/scripts/train.py
+++ week_07_decoder_centric_models/scripts/train.py
@@ -11,13 +11,24 @@ from src.data.dataset import SummarizationDataModule
 from src.models.summarizer import SummarizationModule
 from transformers import AutoTokenizer
 
+import sys 
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
+
+from seed import set_seed
+import torch 
+
 ## DON'T FORGET TO SET SEED
 @hydra.main(config_path = '../configs', config_name = 'config', version_base = '1.3')
 def main(cfg: DictConfig): 
+    set_seed(cfg.seed)
+
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name)
     tokenizer.pad_token = tokenizer.eos_token 
 
     data_module = SummarizationDataModule(cfg.datamodule, tokenizer = tokenizer)
+
+    print('Data module instantiated!')
 
     data_module.prepare_data()
     data_module.setup(stage = 'fit')
@@ -28,12 +39,19 @@ def main(cfg: DictConfig):
     model = SummarizationModule(cfg.model, tokenizer = tokenizer)
     callbacks = [instantiate(cb) for cb in cfg.callbacks]
 
+    print('Model instantiated!') 
+
     trainer = Trainer(
         **cfg.trainer, 
         callbacks = callbacks
     )
 
     trainer.fit(model, datamodule = data_module)
+
+    if cfg.save_model.save_model: 
+        save_path = os.path.join(cfg.save_model.save_path, f'{cfg.save_model.model_name}.pt')
+        torch.save(model.state_dict(), save_path) 
+        print(f'Saved model state_dict to: {save_path}')
 
 if __name__ == '__main__': 
     main()
