b6a37682473ca49d0fd427ffbe6f417353ba18b6
Ryan McNamara <rjm2232@columbia.edu>
2025-07-14 00:02:47 +0000

    Update evaluate to run correctly using article ids

--- week_07_decoder_centric_models/scripts/evaluate.py
+++ week_07_decoder_centric_models/scripts/evaluate.py
@@ -8,6 +8,9 @@ from src.utils.save import *
 from src.data.dataset import SummarizationDataModule
 from src.models.summarizer import SummarizationModule
 
+from torch.utils.data import DataLoader, Subset
+from src.data.collators import TestCollator
+
 import sys 
 import os 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
@@ -30,20 +33,31 @@ def main(cfg: DictConfig):
     data_module = SummarizationDataModule(cfg.datamodule, tokenizer = tokenizer)
     data_module.setup(stage = 'test') 
     test_dataloader = data_module.test_dataloader()
+
+    # #CHECK 
+    # subset = Subset(test_dataloader.dataset, list(range(100)))
+    # test_collate_fn = TestCollator(tokenizer = tokenizer)
+    # small_dataloader = DataLoader(subset, batch_size = 8, collate_fn = test_collate_fn, shuffle = False)
+    # # CHECK
+
     print('Data module instantiated!') 
 
-    model = SummarizationModule(cfg.model, cfg.optimizer, cfg.scheduler, tokenizer = tokenizer)
+    summarization_module = SummarizationModule(cfg.model, cfg.optimizer, cfg.scheduler, tokenizer = tokenizer)
 
     model_path = os.path.join(cfg.save_model.save_path, cfg.save_model.model_name + '.pt')  
     if not os.path.exists(model_path):
         raise FileNotFoundError(f'Model not found at {model_path}')
     else: 
-        model.load_state_dict(torch.load(model_path))
-        model.to(device) 
+        state_dict = torch.load(model_path, map_location = device)  # Recommended
+        summarization_module.load_state_dict(state_dict, strict = True)  # Add strict=True for safety
+        model = summarization_module.model
+        model.to(device)
+        model.eval() 
 
     print('Model loaded!') 
 
     save_path = os.path.join(cfg.paths.log_dir, 'test')
+    os.makedirs(save_path, exist_ok = True)
 
     chunk_summaries = generate_summaries( 
         cfg = cfg.generation, 
