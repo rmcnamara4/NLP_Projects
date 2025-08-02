75de27b94c31dd5057e6eae85c8f6f683f7b282f
rmcnamara4 <rjm2232@columbia.edu>
2025-08-02 05:22:14 +0000

    Fix the re.sub function to sub with ''

--- week_09_llm_scaling_and_prompt_eng/src/utils/extract.py
+++ week_09_llm_scaling_and_prompt_eng/src/utils/extract.py
@@ -1,7 +1,7 @@
 import re
 
 def extract_final_answer(text): 
-    text = re.sub(r'<<.*>?>>', text) 
+    text = re.sub(r'<<.*>?>>', '', text) 
     numbers = re.findall(r'\d+', text) 
     
     if numbers: 
