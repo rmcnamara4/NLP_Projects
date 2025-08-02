caabe99a03a520646dab8274258c63a2c12d28e5
Ryan McNamara <rjm2232@columbia.edu>
2025-08-01 18:14:07 -0700

    Create function to extract final answer from generated text

---
+++ week_09_llm_scaling_and_prompt_eng/src/utils /extract.py
@@ -0,0 +1,9 @@
+import re
+
+def extract_final_answer(text): 
+    text = re.sub(r'<<.*>?>>', text) 
+    numbers = re.findall(r'\d+', text) 
+    
+    if numbers: 
+        return int(numbers[-1])
+    return None
