4d674616f06e39af3f71c3e03850bba024149f18
Ryan McNamara <rjm2232@columbia.edu>
2025-08-01 20:37:38 -0700

    Update to include full generated text

--- week_09_llm_scaling_and_prompt_eng/src/evaluation/save.py
+++ week_09_llm_scaling_and_prompt_eng/src/evaluation/save.py
@@ -2,11 +2,12 @@ import pandas as pd
 from datetime import datetime
 import os
 
-def save_results(model_name, prompt_version, questions, answers, predictions, correct_vec, output_file): 
+def save_results(model_name, prompt_version, questions, answers, generated_text, predictions, correct_vec, output_file): 
     result = pd.DataFrame({
         'example_id': [f'{i:04d}' for i in range(len(questions))], 
         'question': questions, 
         'expected_answer': answers, 
+        'generated_text': generated_text,
         'model_prediction': predictions, 
         'is_correct': correct_vec, 
         'model_name': [model_name] * len(questions), 
