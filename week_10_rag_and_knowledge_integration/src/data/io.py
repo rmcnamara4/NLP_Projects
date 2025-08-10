8537776f8fd0e6ed180a25442eae524302c3a811
Ryan McNamara <rjm2232@columbia.edu>
2025-08-10 14:20:15 -0700

    Make the client creation more robust by adding access keys

--- week_10_rag_and_knowledge_integration/src/data/io.py
+++ week_10_rag_and_knowledge_integration/src/data/io.py
@@ -8,7 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 S3_BUCKET = os.getenv('S3_BUCKET')
-s3_client = boto3.client('s3')
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name = os.getenv('AWS_DEFAULT_REGION', 'us-east-2')
+)
 
 def save_jsonl(records: Iterable[Dict], filename: str, use_s3: Optional[bool] = True): 
     if use_s3: 
