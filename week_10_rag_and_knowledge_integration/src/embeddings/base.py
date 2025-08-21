5cb28d236b4495738abf66b660609fc2e4014560
Ryan McNamara <rjm2232@columbia.edu>
2025-08-20 17:11:13 -0700

    Create script to hold the structure of my embeddings class

---
+++ week_10_rag_and_knowledge_integration/src/embeddings/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import List
+import numpy as np
+
+class Embeddings(ABC): 
+    @abstractmethod 
+    def embed_text(self, texts: List[str], batch_size: int = 32) -> np.ndarray: 
+        pass
+
+    @abstractmethod
+    def embed_query(self, query: str) -> np.ndarray:
+        pass
+
+    @property
+    @abstractmethod
+    def dim(self) -> int: 
+        pass
