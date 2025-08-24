3f55785e698d7f94540a121de9850125c9feffde
Ryan McNamara <rjm2232@columbia.edu>
2025-08-24 16:19:39 -0700

    Fix the double loop when looping through the records

--- week_10_rag_and_knowledge_integration/src/vectorstore/pipeline.py
+++ week_10_rag_and_knowledge_integration/src/vectorstore/pipeline.py
@@ -1,18 +1,17 @@
 from typing import Dict, Iterable
 
 def create_index(records: Iterable[Dict], embedder, store, batch_size: int = 32) -> None: 
+    texts, metadatas = [], []
     for r in records: 
-        texts, metadatas = [], []
-        for r in records: 
-            texts.append(r['text'])
-            metadatas.append(
-                {k: r.get(k, None) for k in r.keys() if k != 'text'}
-            )
-            if len(texts) >= batch_size: 
-                embeddings = embedder.embed_text(texts, batch_size = batch_size) 
-                store.add(embeddings, metadatas)
-                texts, metadatas = [], []
-
-        if texts: 
+        texts.append(r['text'])
+        metadatas.append(
+            {k: r.get(k, None) for k in r.keys() if k != 'text'}
+        )
+        if len(texts) >= batch_size: 
             embeddings = embedder.embed_text(texts, batch_size = batch_size) 
             store.add(embeddings, metadatas)
+            texts, metadatas = [], []
+
+    if texts: 
+        embeddings = embedder.embed_text(texts, batch_size = batch_size) 
+        store.add(embeddings, metadatas)
